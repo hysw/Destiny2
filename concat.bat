1d3a4c6d6cd18202944e037d29c62e29168aaa30
Peter Chen <hyswpc@gmail.com>
2023-03-09 17:08:02 -0500

    update

---
+++ concat.bat
@@ -0,0 +1,6 @@
+type ^
+  src\title.txt src\empty-line.txt ^
+  src\trivial-wip.txt src\empty-line.txt ^
+  src\trivial-trash.txt src\empty-line.txt ^
+  download\choosy_voltron.txt src\empty-line.txt ^
+  > wishlist.txt
---
+++ download.bat
@@ -0,0 +1,2 @@
+mkdir download
+curl -o download\choosy_voltron.txt https://raw.githubusercontent.com/48klocs/dim-wish-list-sources/master/choosy_voltron.txt

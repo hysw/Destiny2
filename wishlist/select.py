16cfb10fc4fca8356ce6be8c6360ba10610461ab
Peter Chen <hyswpc@gmail.com>
2024-07-05 00:07:08 -0400

    Update

--- wishlist/select.py
+++ wishlist/select.py
@@ -20,13 +20,16 @@ def filter_lines(lines):
         if check_note(line):
           output.append(line)
   return output
+title_lines = """title: Trivial's subset of voltron
+description:https://raw.githubusercontent.com/hysw/Destiny2/main/wishlist/voltron_subset.txt
 
+"""
 lines = []
 with open(sys.argv[1], encoding="utf-8") as listfile:
   lines = [line.strip("\n") for line in listfile]
 lines = filter_lines(lines)
 with open(sys.argv[2], encoding="utf-8", mode="w") as listfile:
-  listfile.write("title: Trivial's subset of voltron\n")
+  listfile.write(title_lines)
   for line in lines:
     listfile.write(line)
     listfile.write("\n")
